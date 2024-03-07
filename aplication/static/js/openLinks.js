b384f1a0e27f3544dad707a855cc3ede3834d1dc
Vitor Daniel <vitordanieldeveloper@gmail.com>
2024-03-07 16:01:00 -0300

    Refactoring the openLinks JS code

--- aplication/static/js/openLinks.js
+++ aplication/static/js/openLinks.js
@@ -1,17 +1,52 @@
 const button = document.querySelector("#open-links")
 
-function toggleDiv() {
-    var div = document.querySelector(".profile-socials");
+function cleanInnerDivButton(){
+    button.innerHTML = ''
+}
+
+function getcomputedStyle(div = null) {
     var computedStyle = window.getComputedStyle(div);
+    return computedStyle
+}
+
+function createParagraph(){
+    p = document.createElement("p")
+    return p
+}
+
+function openDiv(div = null){
+    div.classList.remove("animation-reverse-items");
+    div.style.display = "flex"
+}
+
+function closeDiv(div = null){
+    div.classList.remove('animation-items');
+    div.classList.add("animation-reverse-items");
+    setTimeout(() => {
+        div.style.display = "none";
+    }, 300); 
+}
+
+function changeTextParagraph(paragraph = null, text){
+    paragraph.innerHTML = text
+}
+
+function appendChild(paragraph = null){
+    button.appendChild(paragraph)
+}
+
+function toggleDiv() {
+    cleanInnerDivButton()
+    p = createParagraph()
+    var div = document.querySelector(".profile-socials");
+    var computedStyle = getcomputedStyle(div)
     if (computedStyle.display === "none") {
-        div.classList.remove("animation-reverse-items");
-        div.style.display = "flex";
+        openDiv(div)
+        changeTextParagraph(p, 'Esconder Contatos')
     } else {
-        div.classList.remove('animation-items');
-        div.classList.add("animation-reverse-items");
-        setTimeout(() => {
-            div.style.display = "none";
-        }, 300); 
+        closeDiv(div)
+        changeTextParagraph(p, 'Mostrar Contatos')
     }
+    appendChild(p)
 }
 
