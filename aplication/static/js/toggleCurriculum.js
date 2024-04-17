3652682adfe98a152d8ba19fd12f714980f58bc8
Vitor Daniel <vitordanieldeveloper@gmail.com>
2024-04-17 14:58:31 -0300

    Solving bug off modal curriculum button animation, and making the button responsive

--- aplication/static/js/toggleCurriculum.js
+++ aplication/static/js/toggleCurriculum.js
@@ -1,27 +1,13 @@
 const closeModalButton = document.querySelector("#close-curriculum");
 const info = document.querySelector("#info-project")
-const fade = document.querySelector("#fade")
+const fade = document.querySelector("#fade-about")
 const principal = document.querySelector(".rightPages#about")
 const left = document.querySelector(".left#about")
 
-
-function showBackground(){
-    principal.style.height = "";
-    principal.style.opacity = "100%"
-    left.style.display = "flex";
-  }
-
-const toggleModal = (openModalButton = null) => {
-    if (openModalButton) {
-      if (window.innerWidth < 1024){
-        hideBackground()
-      }
-    } else {
-      showBackground()
-    }
-    [info, fade].forEach((el)=> el.classList.toggle("hide"))
-  }
+const toggleModal = () => {
+  [info, fade].forEach((el)=> el.classList.toggle("hide"))
+}
   
-  [closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-  });
+[closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
